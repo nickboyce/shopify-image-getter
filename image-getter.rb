1901bb9edaf390996aad3dbaf5aa90c6c2ddc15e
Nick Boyce <nick@nickboyce.com>
2011-06-16 18:23:37 +0100

    now gets 250 items at a time

--- image-getter.rb
+++ image-getter.rb
@@ -7,18 +7,17 @@ require 'net/http'
 @shopifyAPIKey = "b05b2f4088c15a88cf75a2a72f318ac2"
 @shopifyAPIPassword = "98549011183527258f8d6d45a8f100e9"
 @shopifyShopURL = "bholu.myshopify.com"
+@sizeArray = ["pico", "icon", "thumb", "small", "compact", "medium", "large", "grande", "original"]
 
-sizeArray = ["pico", "icon", "thumb", "small", "compact", "medium", "large", "grande", "original"]
-
-def saveXML
+def saveXML(page=1)
   Net::HTTP.start(@shopifyShopURL) {|http|
-    req = Net::HTTP::Get.new("/admin/products.xml")
+    req = Net::HTTP::Get.new("/admin/products.xml?limit=250&page=2")
     req.basic_auth @shopifyAPIKey, @shopifyAPIPassword
     response = http.request(req)
     # puts response.body into the file products.xml
     open("products.xml", 'wb') do |file|
       file << response.body
-    end  
+    end
   }
 end
 
@@ -32,23 +31,34 @@ def getURL(url, size="")
     url
   end
 end
-# get the XML
-saveXML()
-doc = Hpricot(open("products.xml"))
-doc.search("//image/src").each do |i|
-  url = i.inner_text
-  # puts getURL(url, "compact")
-  filename = url[url.rindex("/")+1..url.rindex("?")-1]
-  sizeArray.each do |theSize|
-    # create directory if it doesn't exist
-    unless File::directory?(theSize) then 
-      Dir::mkdir(theSize)
-      puts "Creating directory: #{theSize}"
-    end
-    puts "getting " + getURL(url, theSize)
-    # save the file
-    open(theSize + "/" + getURL(filename, theSize), 'wb') do |file|
-      file << open(getURL(url, theSize)).read
+
+def parseXML(doc)
+  doc.search("//image/src").each do |i|
+    url = i.inner_text
+    # puts getURL(url, "compact")
+    filename = url[url.rindex("/")+1..url.rindex("?")-1]
+    @sizeArray.each do |theSize|
+      # create directory if it doesn't exist
+      unless File::directory?(theSize) then 
+        Dir::mkdir(theSize)
+        puts "Creating directory: #{theSize}"
+      end
+      
+      filePath = "#{theSize}/#{getURL(filename, theSize)}"
+      # save the file if it doesn't exist
+      if !FileTest.exist?(filePath)
+        puts "getting: #{getURL(url, theSize)}"
+        open(filePath, 'wb') do |file|
+          file << open(getURL(url, theSize)).read
+        end
+      else
+        puts "exists: #{filePath}"
+      end
     end
   end
 end
+
+# get the XML
+saveXML(2)
+doc = Hpricot(open("products.xml"))
+parseXML(doc);
